4c3d0ed7f2810aa9ee963e619f3cd161ac26cc5e
thariqramadhan <athariqramadhan@yahoo.com>
2019-07-08 16:02:19 +0800

    8 th July

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'form-data', pathMatch: 'full' },
+  { path: '', redirectTo: 'report-problem', pathMatch: 'full' },
   // Uncomment to change to home page
   // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'maintenance-records', loadChildren: './maintenance-records/maintenance-records.module#MaintenanceRecordsPageModule' },
   { path: 'report-problem', loadChildren: './report-problem/report-problem.module#ReportProblemPageModule' },
-  { path: 'form-data', loadChildren: './form-data/form-data.module#FormDataPageModule' },
 ];
 
 @NgModule({
--- src/app/report-problem/report-problem.page.ts
+++ src/app/report-problem/report-problem.page.ts
@@ -10,6 +10,14 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReportProblemPage implements OnInit {
 
+  constructor(public alertController: AlertController, private http: HttpClient, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      image: [''],
+    });
+  }
+
   uploadForm: FormGroup;
 
   File: any = [];
@@ -17,7 +25,6 @@ export class ReportProblemPage implements OnInit {
   Email: string = "johnny258147@gmail.com";
   ErrorType = 0;
   Description: string = '';
-
 
   problems = [
     { problem: 'Button does not respond' },
@@ -27,18 +34,10 @@ export class ReportProblemPage implements OnInit {
     { problem: 'Other' }
   ];
 
+  updateTrack: boolean = false;
+
   public selected: string;
   public type;
-
-
-  constructor(public alertController: AlertController, private http: HttpClient, private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.uploadForm = this.formBuilder.group({
-      image: [''],
-    });
-  }
-
 
   public toggle(selected, type) {
     this.ErrorType = type + 1;
@@ -54,42 +53,85 @@ export class ReportProblemPage implements OnInit {
 
   async submit() {
     if (this.ErrorType == 0) {
-      console.log("You must fill problem")
-    } else {
-
-      const reportProblems = new FormData();
-
-      reportProblems.append('File', this.uploadForm.get('image').value);
-      reportProblems.append('Device_ID', '1');
-      reportProblems.append('Email', 'hehe@gmail.com');
-      reportProblems.append('ErrorType', String(this.ErrorType));
-      reportProblems.append('Description', '');
-
-      console.log(this.uploadForm);
-
-      const thank = await this.alertController.create({
+      const error = await this.alertController.create({
         mode: "ios",
-        header: 'Thank you for your assistance!',
-        message: 'We have received a problem report',
+        header: 'Dispenser problem is incorret',
+        message: 'Please choose one of the problems above!',
         buttons: [
           {
             text: 'OK',
-            handler: (blah) => {
-              console.log('Confirm Cancel: blah');
+            handler: () => {
+              console.log('Confirm Cancel: Ok');
             }
           }
         ]
       });
+      await error.present();
 
-      await thank.present();
 
+    } else {
 
-      this.http.post<any>("https://smartcampus.et.ntust.edu.tw:5425/Dispenser/Report", reportProblems)
-        .subscribe(data => {
-          console.log(data);
-        }, error => {
-          console.log(error);
+      if ((this.Description == '') && (this.ErrorType == 5)) {
+        const error = await this.alertController.create({
+          mode: "ios",
+          header: 'Dispenser problem is left blank',
+          message: 'Please fill the description when choose other option!',
+          buttons: [
+            {
+              text: 'OK',
+              handler: () => {
+                console.log('Confirm Cancel: Ok');
+              }
+            }
+          ]
         });
+        await error.present();
+
+      } else {
+
+        const reportProblems = new FormData();
+        reportProblems.append('File', this.uploadForm.get('image').value);
+        reportProblems.append('Device_ID', String(this.Device_ID));
+        reportProblems.append('Email', String(this.Email));
+        reportProblems.append('ErrorType', String(this.ErrorType));
+        reportProblems.append('Description', String(this.Description));
+
+        const thank = await this.alertController.create({
+          mode: "ios",
+          header: 'Thank you for your assistance!',
+          message: 'We have received a problem report',
+          buttons: [
+            {
+              text: 'OK',
+              handler: () => {
+                console.log('Confirm Cancel: Ok');
+              }
+            }
+          ]
+        });
+        await thank.present();
+
+        this.http.post<any>("https://smartcampus.et.ntust.edu.tw:5425/Dispenser/Report", reportProblems)
+          .subscribe(data => {
+            console.log(data);
+          }, error => {
+            console.log(error);
+          });
+        if (this.updateTrack == true) {
+          let updateData = {
+            'Email': this.Email,
+            'Device_ID': this.Device_ID,
+            'Status': 1
+          }
+          this.http.post<any>("https://smartcampus.et.ntust.edu.tw:5425/Dispenser/Track", updateData)
+            .subscribe(data => {
+              console.log(data);
+            }, error => {
+              console.log(error);
+            });
+        }
+
+      }
     }
   }
   onKey(event: any) {
@@ -98,7 +140,6 @@ export class ReportProblemPage implements OnInit {
     }
     this.problems[4]['isChecked'] = 1;
     this.ErrorType = 5;
-
   }
 
   async AlertConfirm() {
@@ -109,8 +150,8 @@ export class ReportProblemPage implements OnInit {
       buttons: [
         {
           text: 'Cancel',
-          handler: (blah) => {
-            console.log('Confirm Cancel: blah');
+          handler: () => {
+            console.log('Confirm Cancel');
           }
         }, {
           text: 'Discard',
@@ -131,7 +172,6 @@ export class ReportProblemPage implements OnInit {
       const file = event.target.files[0];
       this.uploadForm.get('image').setValue(file);
     }
-    console.log("jhaha");
   }
 
 }
